d39a09fa47bd545b9de9f02e3ef61a4654313590
snkv04 <niketh.vangaru@gmail.com>
2025-06-10 22:01:03 -0700

    Added 2102D and 2116D

--- Old/CF2102D.cpp
+++ Old/CF2102D.cpp
@@ -14,63 +14,156 @@ void print_container(const Container &c, string prefix = "", std::ostream &os = 
     os << "]" << endl;
 }
 
+struct SegmentTree {
+private:
+    int n;
+    vector<ll> t;
+    vector<int> a;
+
+public:
+    SegmentTree(const vector<int> &arr) {
+        n = arr.size();
+        a = arr;
+        t.resize(4*n);
+        fill(t.begin(), t.end(), 0);
+        build(1, 0, n-1);
+    }
+
+    void build(int v, int l, int r) {
+        if (l == r) {
+            t[v] = a[l];
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        build(2*v, l, m);
+        build(2*v + 1, m+1, r);
+        t[v] = t[2*v] + t[2*v + 1];
+    }
+
+    ll _query(int v, int l, int r, int ql, int qr) {
+        if (l > qr || r < ql) {
+            return 0;
+        }
+        if (l >= ql && r <= qr) {
+            return t[v];
+        }
+
+        int m = l + (r - l) / 2;
+        return _query(2*v, l, m, ql, qr)
+            + _query(2*v+1, m+1, r, ql, qr);
+    }
+
+    ll query(int ql, int qr) {
+        return _query(1, 0, n-1, ql, qr);
+    }
+
+    void _update(int v, int l, int r, int idx, int val) {
+        if (l == r) {
+            t[v] = val;
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        if (idx <= m) {
+            _update(2*v, l, m, idx, val);
+        } else {
+            _update(2*v+1, m+1, r, idx, val);
+        }
+        t[v] = t[2*v] + t[2*v+1];
+    }
+
+    void update(int idx, int val) {
+        _update(1, 0, n-1, idx, val);
+    }
+};
+
+ll count_inversions(vector<int> &v) {
+    map<int, int> mp;
+    int n = v.size();
+    for (int i = 0; i < n; ++i) {
+        mp[v[i]] = i;
+    }
+
+    vector<int> sorted = v;
+    sort(sorted.begin(), sorted.end());
+    SegmentTree st(vector<int>(n, 0));
+    ll inversions = 0;
+    for (int i = 0; i < n; ++i) {
+        int idx = mp[sorted[i]];
+        st.update(idx, 1);
+        if (idx < n-1) 
+            inversions += st.query(idx+1, n-1);
+    }
+    return inversions;
+}
+
 void solve() {
+    /*
+    notes:
+    - since all the elements are distinct, swapping any two elements that are
+    adjacent will change the number of inversions by exactly 1. so, swapping
+    any two adjacent pairs of adjacent elements will change the number of inversions
+    by 0, 2, or 4, meaning that the parity of inversions stays invariant.
+
+    generalizable rules?
+    - sorting and inversions are closely related topics. aside from thinking
+    of sorting as making every pair of adjacent elements such that the left
+    is leq the right, it can also be thought of as eliminating inversions.
+    so, when dealing with problems that involve sorting, take a moment to think
+    about if it connects to inversions
+    - i came up with a way to count the number of inversions in an array with
+    distinct elements in O(n log n), and i'm not sure if this idea has been done
+    before (though something similar surely has). basically, map each element
+    to its index, and then iterate through the elements in ascending order.
+    for each element, we know the amount of elements that we've already iterated
+    through, and those elements are all less than the current element, meaning
+    that they SHOULD be to the left of the current element if they aren't
+    inversions. so, for each element, first "activate" it (find its index and
+    set its value from 0 to 1 in a separate segment tree), and then use the
+    segment tree to find the number of activated elements to the right of it.
+    this way, we find the number of inversions in the form of pairs (i, j)
+    where j is the current element and i is an element less than the current
+    element, and this avoids double counting.
+    */
+
     int n;
     cin >> n;
-    if (true) {
-        vector<int> even = {0}, odd = {0};
-        unordered_map<int, int> idx;
-        for (int i = 1; i <= n; ++i) {
-            int ai;
-            cin >> ai;
-            idx[ai] = i;
-            if (i % 2) {
-                odd.push_back(ai);
-            } else {
-                even.push_back(ai);
-            }
+    vector<int> even, odd, before(n, 0);
+    for (int i = 0; i < n; ++i) {
+        int ai;
+        cin >> ai;
+        before[i] = ai;
+        if (i % 2) {
+            odd.push_back(ai);
+        } else {
+            even.push_back(ai);
         }
-        sort(odd.begin(), odd.end());
-        sort(even.begin(), even.end());
-
-        vector<int> result = {0};
-        for (int i = 1; i <= n; ++i) {
-            int lowest;
-            if (i % 2) {
-                lowest = odd[(i+1)/2];
-            } else {
-                lowest = even[(i+1)/2];
-            }
-            result.push_back(lowest);
-        }
-        // print_container(result, "result = ");
-        // print_container(even, "even = ");
-        // print_container(odd, "odd = ");
-
-        // bool last2orderedeven = idx[even[even.size()-2]] < idx[odd[odd.size()-2]];
-        // bool last2orderedodd = idx[even[even.size()-1]] < idx[odd[odd.size()-1]];
-        // // cout << "last2orderedeven = " << last2orderedeven << ", last2orderedodd = " << last2orderedodd << "\n";
-        // if (last2orderedeven != last2orderedodd) {
-        //     swap(result[n], result[n-2]);
-        // }
-
-        for (int i = 1; i <= n; ++i) {
-            cout << result[i] << " ";
-        }
-        cout << "\n";
-    } else {
-        vector<pair<int, int>> v;
-        for (int i = 0; i < n; i += 2) {
-            int a, b;
-            cin >> a >> b;
-            v.push_back(make_pair(a, b));
-        }
-        sort(v.begin(), v.end());
-        for (auto p : v) {
-            cout << p.first << " " << p.second << " ";
-        }
-        cout << "\n";
     }
+    sort(odd.begin(), odd.end());
+    sort(even.begin(), even.end());
+
+    vector<int> result;
+    for (int i = 0; i < n; ++i) {
+        int lowest;
+        if (i % 2) {
+            lowest = odd[i/2];
+        } else {
+            lowest = even[i/2];
+        }
+        result.push_back(lowest);
+    }
+
+    ll inversionsbefore = count_inversions(before),
+        inversionsafter = count_inversions(result);
+    if (inversionsbefore % 2 != inversionsafter % 2) {
+        swap(result[n-1], result[n-3]);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cout << result[i] << " ";
+    }
+    cout << "\n";
 }
 
 int main() {
--- Old/CF2116D.cpp
+++ Old/CF2116D.cpp
@@ -15,9 +15,46 @@ void print_container(const Container &c, string prefix = "", std::ostream &os = 
 }
 
 void solve() {
+    /*
+    notes:
+    - when undoing operations, we can place lower bounds on what the input values
+    were; that much is pretty clear. however, there are other restrictions that
+    can be placed, such as: two values are both greater than or equal to 3, but
+    one of them must have been equal to 3, because the output of the min operation
+    was 3. basically, what the solution is doing here is ignoring such restrictions
+    and assuming that only the min restrictions apply, so that the full state
+    at the current step is valid, and then everything is checked at the end. another
+    example is if x, y, and z are all distinct, and we end up with an operation
+    which tells us that the min of 3 and 5 was equal to 2; in this case, we'd just
+    gleefully assume (for each of 3 and 5) that the other element was responsible
+    for the minimum and update 3 to be max(3, 2) (as well as updating 5 to be
+    max(5, 2)), and then check our assumptions at the end.
+
+    generalizable ideas?
+    - basically, there are two ways to solve this problem (with the first one
+    being much more complex and, possibly, computationally infeasible). we know
+    that what we're trying to do is see if an end state is possible, and if it is,
+    then we want to give a possible start state for that end state. both ways involve
+    going backward from the end state. the first is to keep track of all current
+    and past restraints, and as soon as any of them are "broken" (meaning that
+    we are now forcing the array to be in a situation that is not consistent with
+    a past constraint), then we know that the sequence of operations is impossible,
+    otherwise we go all the way and output the array after undoing all the ops.
+    the second is to assume at EACH OPERATION that the output of the operation
+    is possible and to force the inputs to make that output possible, not worrying
+    about if it's actually possible or about past (or future, since we're going
+    backwards) operations, and then go through all the operations again and see
+    if the final output is what it's supposed to be. the second is more easily
+    implementable, and is also correct. (why, specifically?)
+    - more generally, perhaps, instead of going through all operations and checking
+    the conditions placed by the current and past ones, we just go through the
+    operations only worrying about the current, then do a final pass through all
+    to make sure that the whole sequence of operations is still valid.
+    */
+
     int n, q;
     cin >> n >> q;
-    int c[n]; for (int i = 0; i < n; ++i) cin >> c[i];
+    vector<int> b(n); for (int i = 0; i < n; ++i) cin >> b[i];
     int queries[q][3];
     for (int i = 0; i < q; ++i) {
         for (int j = 0; j < 3; ++j) {
@@ -26,50 +63,33 @@ void solve() {
         }
     }
 
+    vector<int> c = b;
     for (int i = q-1; i >= 0; --i) {
         int x=queries[i][0], y=queries[i][1], z=queries[i][2];
-        if (z != x && z != y) {
-            if (x == y) {
-                if (c[x] == -1) {
-                    c[x] = c[z];
-                } else if (c[z] == -1) {
-                    c[z] = c[x];
-                } else {
-                    if (c[z] != c[x]) {
-                        cout << "-1\n";
-                        return;
-                    } else {
-                        // all good
-                    }
-                }
-            } else {
-                //
-            }
-            if (c[z] = min(c[x], c[y])) {
-                c[z] = -1;
-            } else {
-                cout << "-1\n";
-                return;
-            }
-        } else { // z in [x, y]
-            if (x == y) {
-                // nothing to do
-            } else {
-                if (z == y) {
-                    swap(x, y);
-                }
-                // z == x, x != y
-                if (c[x] == -1 && c[y] == -1) {
-                    
-                } else if (c[x] == -1) {
-                    //
-                } else if (c[y] == -1) {
-                    //
-                } else {
-                    //
-                }
-            }
+        int output = c[z];
+        c[z] = 0;
+        c[x] = max(c[x], output);
+        c[y] = max(c[y], output);
+    }
+
+    vector<int> a = c;
+    for (int i = 0; i < q; ++i) {
+        int x=queries[i][0], y=queries[i][1], z=queries[i][2];
+        c[z] = min(c[x], c[y]); 
+    }
+
+    bool works = true;
+    for (int i = 0; i < n; ++i) {
+        if (c[i] != b[i]) {
+            works = false;
+            break;
         }
+    }
+    if (works) {
+        for (int i = 0; i < n; ++i) cout << a[i] << " ";
+        cout << "\n";
+    } else {
+        cout << "-1\n";
     }
 }
 
@@ -79,7 +99,7 @@ int main() {
     cout.tie(nullptr);
 
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while (t--) {
         solve();
     }
