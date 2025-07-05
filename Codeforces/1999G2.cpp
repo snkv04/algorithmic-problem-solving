cb6e2e80fd844eedc2beba51fed4faa69886c0bb
snkv04 <niketh.vangaru@gmail.com>
2025-07-04 19:24:38 -0700

    Added 1999G2

---
+++ Codeforces/1999G2.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename Container>
+void print_container(const Container &c, string prefix = "", std::ostream &os = std::cout) {
+    os << prefix << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]" << endl;
+}
+
+struct SegmentTree {
+private:
+    int n;
+    vector<ll> t;
+    vector<int> a;
+
+public:
+    SegmentTree(const vector<int> &arr) {
+        n = arr.size();
+        a = arr;
+        t.resize(4 * n);
+        fill(t.begin(), t.end(), 0);
+        build(1, 0, n - 1);
+    }
+
+    void build(int v, int l, int r) {
+        if (l == r) {
+            t[v] = a[l];
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        build(2 * v, l, m);
+        build(2 * v + 1, m + 1, r);
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+
+    ll _query(int v, int l, int r, int ql, int qr) {
+        if (r < ql || qr < l) {
+            return 0;
+        }
+        if (ql <= l && r <= qr) {
+            return t[v];
+        }
+
+        int m = l + (r - l) / 2;
+        return _query(2 * v, l, m, ql, qr)
+            + _query(2 * v + 1, m + 1, r, ql, qr);
+    }
+
+    ll query(int ql, int qr) {
+        return _query(1, 0, n - 1, ql, qr);
+    }
+
+    void _update(int v, int l, int r, int idx, int val) {
+        if (l == r) {
+            t[v] = val;
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        if (idx <= m) {
+            _update(2 * v, l, m, idx, val);
+        } else {
+            _update(2 * v + 1, m + 1, r, idx, val);
+        }
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+
+    void update(int idx, int val) {
+        _update(1, 0, n - 1, idx, val);
+    }
+};
+
+void solve() {
+    int leftmost = 999;
+    int l = 2, r = 998;
+    while (l <= r) {
+        int m1 = l + (r-l)/3, m2 = r-(r-l)/3;
+        cout << "? " << m1 << " " << m2 << endl;
+
+        int area;
+        cin >> area;
+
+        if (area == m1 * m2) {
+            l = m2 + 1;
+        } else if (area == (m1+1) * (m2+1)) {
+            leftmost = m1;
+            r = m1-1;
+        } else {
+            leftmost = m2;
+            l = m1+1;
+            r = m2-1;
+        }
+    }
+    cout << "! " << leftmost << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
