0eadbe67daf972949ebde9b4deb60f7906652c84
snkv04 <niketh.vangaru@gmail.com>
2025-08-10 13:07:42 -0700

    Add Kattis familyvisits

---
+++ Kattis/familyvisits.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename Container>
+void print_container(const Container &c, string prefix = "", std::ostream &os = std::cout) {
+    os << prefix << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]" << endl;
+}
+
+struct SegmentTree {
+private:
+    int n;
+    vector<ll> t;
+    vector<int> a;
+
+public:
+    SegmentTree(const vector<int> &arr) {
+        n = arr.size();
+        a = arr;
+        t.resize(4 * n);
+        fill(t.begin(), t.end(), 0);
+        build(1, 0, n - 1);
+    }
+
+    void build(int v, int l, int r) {
+        if (l == r) {
+            t[v] = a[l];
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        build(2 * v, l, m);
+        build(2 * v + 1, m + 1, r);
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+
+    ll _query(int v, int l, int r, int ql, int qr) {
+        if (r < ql || qr < l) {
+            return 0;
+        }
+        if (ql <= l && r <= qr) {
+            return t[v];
+        }
+
+        int m = l + (r - l) / 2;
+        return _query(2 * v, l, m, ql, qr)
+            + _query(2 * v + 1, m + 1, r, ql, qr);
+    }
+
+    ll query(int ql, int qr) {
+        return _query(1, 0, n - 1, ql, qr);
+    }
+
+    void _update(int v, int l, int r, int idx, int val) {
+        if (l == r) {
+            t[v] = val;
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        if (idx <= m) {
+            _update(2 * v, l, m, idx, val);
+        } else {
+            _update(2 * v + 1, m + 1, r, idx, val);
+        }
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+
+    void update(int idx, int val) {
+        _update(1, 0, n - 1, idx, val);
+    }
+};
+
+void solve() {
+    int n, d;
+    cin >> n >> d;
+    vector<int> m(n), c(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> m[i] >> c[i];
+    }
+    vector<int> v(d+1);
+    v[0] = -1;
+    for (int i = 1; i <= d; ++i) {
+        cin >> v[i];
+        --v[i];
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= d && ans != -1; ++i) {
+        int start = v[i-1] + 1, end = v[i];
+        int currclean = 0;
+        priority_queue<int, vector<int>> cleans;
+        for (int day = end; day >= start && ans != -1; --day) {
+            int mess = m[day];
+            cleans.push(c[day]);
+
+            while (mess > 0) {
+                if (mess <= currclean) {
+                    currclean -= mess;
+                    mess = 0;
+                    break;
+                } else {
+                    mess -= currclean;
+                    currclean = 0;
+                    if (cleans.empty()) {
+                        ans = -1;
+                        break;
+                    } else {
+                        ++ans;
+                        currclean = cleans.top();
+                        cleans.pop();
+                    }
+                }
+            }
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
