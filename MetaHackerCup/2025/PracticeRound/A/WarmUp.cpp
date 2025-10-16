9d5ed60b83e4b0948f341fbb535f0c591317ba28
snkv04 <niketh.vangaru@gmail.com>
2025-10-15 21:17:39 -0400

    Add A, B, and C from the Meta Hacker Cup 2025 Practice Round

---
+++ MetaHackerCup/2025/PracticeRound/A/WarmUp.cpp
@@ -0,0 +1,184 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename Container>
+void print_container(const Container &c, string prefix = "", std::ostream &os = std::cout) {
+    os << prefix << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]" << endl;
+}
+
+struct SegmentTree {
+private:
+    int n;
+    vector<ll> t;
+    vector<int> a;
+
+public:
+    SegmentTree(const vector<int> &arr) {
+        n = arr.size();
+        a = arr;
+        t.resize(4 * n);
+        fill(t.begin(), t.end(), 0);
+        build(1, 0, n - 1);
+    }
+
+    void build(int v, int l, int r) {
+        if (l == r) {
+            t[v] = a[l];
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        build(2 * v, l, m);
+        build(2 * v + 1, m + 1, r);
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+
+    ll _query(int v, int l, int r, int ql, int qr) {
+        if (r < ql || qr < l) {
+            return 0;
+        }
+        if (ql <= l && r <= qr) {
+            return t[v];
+        }
+
+        int m = l + (r - l) / 2;
+        return _query(2 * v, l, m, ql, qr)
+            + _query(2 * v + 1, m + 1, r, ql, qr);
+    }
+
+    ll query(int ql, int qr) {
+        return _query(1, 0, n - 1, ql, qr);
+    }
+
+    void _update(int v, int l, int r, int idx, int val) {
+        if (l == r) {
+            t[v] = val;
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        if (idx <= m) {
+            _update(2 * v, l, m, idx, val);
+        } else {
+            _update(2 * v + 1, m + 1, r, idx, val);
+        }
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+
+    void update(int idx, int val) {
+        _update(1, 0, n - 1, idx, val);
+    }
+};
+
+struct RollingHash {
+private:
+    vector<ll> P = {998244353, 999999937};
+    vector<ll> MOD = {1000000007, 1000000009};
+    vector<vector<ll>> PPOW;
+    vector<vector<ll>> rolling;
+    string s;
+    int n;
+
+    void _build_hashes() {
+        rolling = vector<vector<ll>>(n + 1, vector<ll>(2, 0));
+        for (int t = 0; t < 2; ++t) {
+            for (int i = 1; i <= n; ++i) {
+                int letter_as_int = s[i - 1];
+                rolling[i][t] = (rolling[i - 1][t] * P[t] + letter_as_int) % MOD[t];
+            }
+        }
+    }
+
+    void _build_powers() {
+        PPOW = vector<vector<ll>>(n + 1, vector<ll>(2, 1));
+        for (int t = 0; t < 2; ++t) {
+            for (int i = 1; i <= n; ++i) {
+                PPOW[i][t] = (PPOW[i - 1][t] * P[t]) % MOD[t];
+            }
+        }
+    }
+
+public:
+    RollingHash(string &s) : s(s), n(s.size()) {
+        _build_hashes();
+        _build_powers();
+    }
+
+    // l and r are both 0-based indices, where l is inclusive and r is exclusive
+    pair<ll, ll> get_hash(int l, int r) {
+        vector<ll> result(2);
+        for (int t = 0; t < 2; ++t) {
+            result[t] = (rolling[r][t] - (rolling[l][t] * PPOW[r - l][t] % MOD[t]) + MOD[t]) % MOD[t];
+        }
+        return make_pair(result[0], result[1]);
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < 2*n; ++i) {
+        if (i < n) cin >> a[i];
+        else cin >> b[i-n];
+    }
+    for (int i = 0; i < n; ++i) {
+        if (a[i] > b[i]) {
+            cout << "-1\n";
+            return;
+        }
+    }
+
+    vector<vector<pair<int, int>>> plates(n+1);
+    for (int i = 0; i < n; ++i) {
+        plates[a[i]].push_back(make_pair(i + 1, b[i]));
+    }
+
+    vector<pair<int, int>> ans;
+    for (int i = 1; i <= n; ++i) {
+        for (pair<int, int> p : plates[i]) {
+            int idx = p.first, target = p.second;
+            if (plates[target].empty()) {
+                cout << "-1\n";
+                return;
+            }
+            if (target != i) {
+                ans.push_back(make_pair(idx, plates[target][0].first));
+            }
+        }
+    }
+
+    cout << ans.size() << "\n";
+    for (pair<int, int> operation : ans) {
+        cout << operation.first << " " << operation.second << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+
+    return 0;
+}
