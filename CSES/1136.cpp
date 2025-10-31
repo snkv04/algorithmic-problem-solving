3773a054a73ef43bb403d0cf4e80b1a6ea013241
snkv04 <niketh.vangaru@gmail.com>
2025-10-31 02:46:19 -0400

    CSES Counting Paths

---
+++ CSES/1136.cpp
@@ -0,0 +1,299 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+int maxpow;
+vector<int> depth, add, sub, pref;
+vector<vector<int>> ancestor, adj;
+vector<bool> visited;
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename Container>
+void print_container(const Container &c, string prefix = "", std::ostream &os = std::cout) {
+    os << prefix << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]" << endl;
+}
+
+struct SegmentTree {
+private:
+    int n;
+    vector<ll> t;
+    vector<int> a;
+
+public:
+    SegmentTree(const vector<int> &arr) {
+        n = arr.size();
+        a = arr;
+        t.resize(4 * n);
+        fill(t.begin(), t.end(), 0);
+        build(1, 0, n - 1);
+    }
+
+    void build(int v, int l, int r) {
+        if (l == r) {
+            t[v] = a[l];
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        build(2 * v, l, m);
+        build(2 * v + 1, m + 1, r);
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+
+    ll _query(int v, int l, int r, int ql, int qr) {
+        if (r < ql || qr < l) {
+            return 0;
+        }
+        if (ql <= l && r <= qr) {
+            return t[v];
+        }
+
+        int m = l + (r - l) / 2;
+        return _query(2 * v, l, m, ql, qr)
+            + _query(2 * v + 1, m + 1, r, ql, qr);
+    }
+
+    ll query(int ql, int qr) {
+        return _query(1, 0, n - 1, ql, qr);
+    }
+
+    void _update(int v, int l, int r, int idx, int val) {
+        if (l == r) {
+            t[v] = val;
+            return;
+        }
+
+        int m = l + (r - l) / 2;
+        if (idx <= m) {
+            _update(2 * v, l, m, idx, val);
+        } else {
+            _update(2 * v + 1, m + 1, r, idx, val);
+        }
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+
+    void update(int idx, int val) {
+        _update(1, 0, n - 1, idx, val);
+    }
+};
+
+struct RollingHash {
+private:
+    vector<ll> P = {998244353, 999999937};
+    vector<ll> MOD = {1000000007, 1000000009};
+    vector<vector<ll>> PPOW;
+    vector<vector<ll>> rolling;
+    string s;
+    int n;
+
+    void _build_hashes() {
+        rolling = vector<vector<ll>>(n + 1, vector<ll>(2, 0));
+        for (int t = 0; t < 2; ++t) {
+            for (int i = 1; i <= n; ++i) {
+                int letter_as_int = s[i - 1];
+                rolling[i][t] = (rolling[i - 1][t] * P[t] + letter_as_int) % MOD[t];
+            }
+        }
+    }
+
+    void _build_powers() {
+        PPOW = vector<vector<ll>>(n + 1, vector<ll>(2, 1));
+        for (int t = 0; t < 2; ++t) {
+            for (int i = 1; i <= n; ++i) {
+                PPOW[i][t] = (PPOW[i - 1][t] * P[t]) % MOD[t];
+            }
+        }
+    }
+
+public:
+    RollingHash(string &s) : s(s), n(s.size()) {
+        _build_hashes();
+        _build_powers();
+    }
+
+    // l and r are both 0-based indices, where l is inclusive and r is exclusive
+    pair<ll, ll> get_hash(int l, int r) {
+        vector<ll> result(2);
+        for (int t = 0; t < 2; ++t) {
+            result[t] = (rolling[r][t] - (rolling[l][t] * PPOW[r - l][t] % MOD[t]) + MOD[t]) % MOD[t];
+        }
+        return make_pair(result[0], result[1]);
+    }
+};
+
+void dfs(int node) {
+    visited[node] = true;
+    for (int next : adj[node]) {
+        if (!visited[next]) {
+            depth[next] = depth[node] + 1;
+            ancestor[next][0] = node;
+            dfs(next);
+        }
+    }
+}
+
+void build_ancestors(int n) {
+    for (int i = 1; i <= maxpow; ++i) {
+        for (int node = 0; node < n; ++node) {
+            int halfway = ancestor[node][i-1];
+            if (halfway == -1) {
+                ancestor[node][i] = -1;
+            } else {
+                ancestor[node][i] = ancestor[halfway][i-1];
+            }
+        }
+    }
+}
+
+int get_ancestor(int node, int k) {
+    if (k > (1 << maxpow)) return -1;
+
+    int curr = node;
+    int exp = 0;
+    while (k) {
+        if (k % 2) {
+            curr = ancestor[curr][exp];
+        }
+        k /= 2;
+        ++exp;
+        if (curr == -1) {
+            return -1;
+        }
+    }
+    return curr;
+}
+
+int get_lca(int a, int b) {
+    if (depth[a] < depth[b]) {
+        int diff = depth[b] - depth[a];
+        b = get_ancestor(b, diff);
+    } else if (depth[b] < depth[a]) {
+        int diff = depth[a] - depth[b];
+        a = get_ancestor(a, diff);
+    }
+
+    if (a == b) {
+        return a;
+    }
+
+    for (int i = maxpow; i >= 0; --i) {
+        if (ancestor[a][i] != ancestor[b][i]) {
+            a = ancestor[a][i];
+            b = ancestor[b][i];
+        }
+    }
+    return ancestor[a][0];
+}
+
+int dfs2(int node) {
+    visited[node] = true;
+    int sum = 0;
+    for (int next : adj[node]) {
+        if (!visited[next]) {
+            sum += dfs2(next);
+        }
+    }
+    sum += add[node];
+    pref[node] = sum;
+    sum -= sub[node];
+    return sum;
+}
+
+void solve() {
+    /*
+    generalizable ideas for the future:
+    - a prefix sum in 1 dimension uses cumulative sums from the first element up to index i for
+    calculating range sums. in a tree, the analogue of the "first element" is any given leaf. every
+    node has a unique path to the root. the difference between a tree and a 1-dimensional line
+    is as follows: in 1 dimension, each element has exactly one next element, but in a tree, each
+    node can have multiple children but each node has exactly one parent.
+        - restated: in 1 dimension, the "first element" is at index 0, and each element has 1 element
+        after it. in a tree, the "first element" is any of the leaves, and each element has 1 element
+        after it.
+    - a path between any two nodes can be broken up into 2 paths that each propagate outward from 
+    (or inward to) the root. this is done using the LCA, so that the two paths are [lca(A, B), ..., A]
+    and [lca(A, B), ..., B]. if A is B's ancestor, then the first path is [A], and the second path
+    is [A, ..., B].
+    - for a given node i, all nodes j in node i's subtree have their path to the root cross i.
+    - why go from leaf to root and not other way around? this way, each node has one element after it.
+    - when breaking a path into 2, to avoid having the path actually count as 2 on the LCA's node, we
+    can shorten one of them to the node right under the LCA.
+    */
+
+    int n, q;
+    cin >> n >> q;
+    adj = vector<vector<int>>(n);
+    for (int i = 0; i < n-1; ++i) {
+        int a, b;
+        cin >> a >> b;
+        --a; --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    maxpow = std::ceil(std::log2(n));
+    ancestor = vector<vector<int>>(n, vector<int>(maxpow+1, -1)); // parent of root is -1
+    depth = vector<int>(n, -1);
+    depth[0] = 0;
+    visited = vector<bool>(n, false);
+    dfs(0); // arbitrarily roots the tree
+    build_ancestors(n);
+
+    add = vector<int>(n, 0);
+    sub = vector<int>(n, 0);
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+        --a; --b;
+        int lca = get_lca(a, b);
+        if (a == b) {
+            add[a] += 1;
+            sub[a] += 1;
+        } else if (lca == a) {
+            add[b] += 1;
+            sub[a] += 1;
+        } else if (lca == b) {
+            add[a] += 1;
+            sub[b] += 1;
+        } else {
+            sub[lca] += 1;
+            add[a] += 1;
+            add[b] += 1;
+            int lca_child = get_ancestor(b, depth[b] - depth[lca] - 1);
+            sub[lca_child] += 1;
+        }
+    }
+    pref = vector<int>(n, 0);
+    visited = vector<bool>(n, false);
+    dfs2(0);
+
+    for (int i = 0; i < n; ++i) {
+        cout << pref[i] << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
