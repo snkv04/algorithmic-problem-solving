9e13b6e40efc32181c950f71a7d9f3581063682e
snkv04 <niketh.vangaru@gmail.com>
2025-12-05 13:55:59 -0500

    Added fraction implementation

---
+++ DSAImplementations/fraction.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+struct Frac {
+    Frac(ll n, ll d) : num(n), denom(d) {
+        reduce();
+    }
+
+    Frac(ll x) : num(x), denom(1) {}
+
+    bool operator<(const Frac &other) const {
+        ll d = lcm(denom, other.denom);
+        ll n1 = num * (d / denom), n2 = other.num * (d / other.denom);
+        return n1 < n2;
+    }
+
+    bool operator==(const Frac &other) const {
+        Frac f1 = *this, f2 = other;
+        f1.reduce(); f2.reduce();
+        return f1.num == f2.num && f1.denom == f2.denom;
+    }
+
+    bool operator!=(const Frac &other) const {
+        return !(operator==(other));
+    }
+
+    bool operator<=(const Frac &other) const {
+        return operator<(other) || operator==(other);
+    }
+
+    void reduce() {
+        ll g = gcd(num, denom);
+        num /= g;
+        denom /= g;
+        if (denom < 0) {
+            num = -num;
+            denom = -denom;
+        }
+    }
+
+    Frac operator+(const Frac &other) {
+        ll d = lcm(denom, other.denom);
+        ll n1 = num * (d / denom), n2 = other.num * (d / other.denom);
+        Frac result(n1 + n2, d);
+        result.reduce();
+        return result;
+    }
+
+    Frac operator-(const Frac &other) {
+        return operator+(Frac(-1) * other);
+    }
+
+    Frac operator*(const Frac &other) {
+        Frac result(num * other.num, denom * other.denom);
+        result.reduce();
+        return result;
+    }
+
+    Frac operator/(const Frac &other) {
+        return operator*({other.denom, other.num});
+    }
+
+    ll sign() {
+        reduce();
+        if (num == 0) return 0;
+        else if (num > 0) return 1;
+        else return -1;
+    }
+
+    friend ostream& operator<<(ostream &os, const Frac &f) {
+        os << "(n=" << f.num << ", d=" << f.denom << ')';
+        return os;
+    }
+
+private:
+    ll num, denom;
+};
