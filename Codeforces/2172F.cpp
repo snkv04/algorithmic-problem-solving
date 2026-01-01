20f47da15d70ded6f1c94cd189da121ca51791ad
snkv04 <niketh.vangaru@gmail.com>
2025-12-31 22:48:02 -0500

    2172F

---
+++ Codeforces/2172F.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<ll> p(n);
+    cin >> p;
+
+    vector<ll> pref(n), suf(n);
+    pref[0] = p[0]; for (int i = 1; i < n; ++i) pref[i] = gcd(p[i], pref[i-1]);
+    suf[n-1] = p[n-1]; for (int i = n-2; i >= 0; --i) suf[i] = gcd(p[i], suf[i+1]);
+
+    ll ans = pref[n-1];
+    for (int i = 1; i < n-1; ++i) ans += min(pref[i], suf[i]);
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
