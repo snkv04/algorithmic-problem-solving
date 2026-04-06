b9b68d9d33d4b469b2eadc4eb3e85d94275db3fb
snkv04 <niketh.vangaru@gmail.com>
2026-04-06 14:00:11 -0400

    1859A

---
+++ Codeforces/CP-31_800/1859A.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cin >> a;
+
+    map<int, int> freq;
+    for (int a_i : a) freq[a_i] += 1;
+
+    if (freq.size() == 1) {
+        cout << "-1\n";
+    } else {
+        vector<int> b, c;
+        auto it = freq.rbegin();
+        for (int i = 0; i < it->second; ++i) c.push_back(it->first);
+        freq.erase(it->first);
+        for (auto [k, v] : freq) for (int i = 0; i < v; ++i) b.push_back(k);
+
+        cout << b.size() << ' ' << c.size() << '\n';
+        for (auto num : b) cout << num << ' ';
+        cout << '\n';
+        for (auto num : c) cout << num << ' ';
+        cout << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
