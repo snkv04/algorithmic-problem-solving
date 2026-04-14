7f72f7fcb45e2e55025f900b9ef82cb82988b7f7
snkv04 <niketh.vangaru@gmail.com>
2026-04-14 16:02:44 -0400

    1861C

---
+++ Codeforces/1861C.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    int minu = 1e9, maxs = 0, len = 0;
+    for (int i = 0; i < n; ++i) {
+        switch (s[i]) {
+            case '0': {
+                minu = min(minu, len);
+                if (len <= 1) {
+                    cout << "NO\n";
+                    return;
+                }
+                break;
+            }
+            case '1': {
+                maxs = max(maxs, len);
+                break;
+            }
+            case '+': {
+                ++len;
+                break;
+            }
+            case '-': {
+                --len;
+                if (len < minu) {
+                    minu = 1e9;
+                }
+                maxs = min(maxs, len);
+                break;
+            }
+        }
+
+        if (minu <= maxs) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
