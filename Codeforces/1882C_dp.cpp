c727bd5f9fbf0eabe3d1470a28b8735be7aa8d55
snkv04 <niketh.vangaru@gmail.com>
2026-04-15 11:13:45 -0400

    1882C

---
+++ Codeforces/1882C_dp.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cin >> a;
+
+    vector<vector<ll>> mem(n + 1, vector<ll>(2, 0));
+    for (int i = n - 1; i >= 0; --i) {
+        mem[i][1] = max(
+            // pick
+            a[i] + max(
+                mem[i + 1][1],
+                i <= n - 2 ? mem[i + 2][1] : 0
+            ),
+            // don't pick
+            mem[i + 1][0]
+        );
+        mem[i][0] = max(
+            // don't pick
+            mem[i + 1][1],
+            // pick
+            mem[i + 1][0]
+        );
+    }
+    cout << mem[0][1] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
