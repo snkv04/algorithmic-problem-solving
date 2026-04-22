c6bf7eb983181c425521d704176577d3ebe8ca59
snkv04 <niketh.vangaru@gmail.com>
2026-04-22 03:02:43 -0400

    CSES 1746

---
+++ CSES/1746.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    cin >> a;
+    for (int i = 0; i < n; ++i) --a[i];
+
+    vector<ll> mem(m, 0);
+    if (a[0] == -1) {
+        fill(mem.begin(), mem.end(), 1);
+    } else {
+        mem[a[0]] = 1;
+    }
+    for (int i = 1; i < n; ++i) {
+        vector<ll> new_mem(m, 0);
+        for (int j = 0; j < m; ++j) {
+            if (a[i] == -1 || a[i] == j) {
+                new_mem[j] = ((j == 0 ? 0 : mem[j - 1]) + mem[j] + (j == m - 1 ? 0 : mem[j + 1])) % MOD;
+            }
+        }
+        mem = std::move(new_mem);
+    }
+
+    ll ans = 0;
+    for (int i = 0; i < m; ++i) ans = (ans + mem[i]) % MOD;
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
