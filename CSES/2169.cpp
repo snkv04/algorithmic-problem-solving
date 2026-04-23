e87d8ae770aef1a88e84f6b1d9d146781f99db20
snkv04 <niketh.vangaru@gmail.com>
2026-04-23 17:21:20 -0400

    CSES 2169

---
+++ CSES/2169.cpp
@@ -0,0 +1,230 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+struct SegmentTree {
+    SegmentTree(int n) : n(n) {
+        tree.resize(4 * n);
+        std::fill(tree.begin(), tree.end(), 0);
+    }
+
+    SegmentTree(const std::vector<int> &arr) : arr(arr), n(arr.size()) {
+        tree.resize(4 * n);
+        std::fill(tree.begin(), tree.end(), 0);
+        build(1, 0, n - 1);
+    }
+
+    void build(int vertex, int left, int right) {
+        if (left == right) {
+            tree[vertex] = arr[left];
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+        build(2 * vertex, left, mid);
+        build(2 * vertex + 1, mid + 1, right);
+        tree[vertex] = tree[2 * vertex] + tree[2 * vertex + 1];
+    }
+
+    long long _query(int vertex, int left, int right, int query_left, int query_right) {
+        if (right < query_left || query_right < left) {
+            return 0;
+        }
+        if (query_left <= left && right <= query_right) {
+            return tree[vertex];
+        }
+
+        int mid = left + (right - left) / 2;
+        return _query(2 * vertex, left, mid, query_left, query_right) +
+            _query(2 * vertex + 1, mid + 1, right, query_left, query_right);
+    }
+
+    long long query(int left, int right) {
+        return _query(1, 0, n - 1, left, right);
+    }
+
+    void _update(int vertex, int left, int right, int idx, long long val) {
+        if (left == right) {
+            tree[vertex] = val;
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+        if (idx <= mid) {
+            _update(2 * vertex, left, mid, idx, val);
+        } else {
+            _update(2 * vertex + 1, mid + 1, right, idx, val);
+        }
+        tree[vertex] = tree[2 * vertex] + tree[2 * vertex + 1];
+    }
+
+    void update(int idx, long long val) {
+        _update(1, 0, n - 1, idx, val);
+    }
+
+    void reset() {
+        fill(tree.begin(), tree.end(), 0);
+    }
+
+private:
+    int n;
+    std::vector<long long> tree;
+    std::vector<int> arr;
+};
+
+struct ByLeft {
+    static bool operator()(const array<int, 3> &a, const array<int, 3> &b) {
+        if (a[1] != b[1]) {
+            return a[1] < b[1];
+        } else {
+            return a[2] > b[2];
+        }
+    }
+};
+
+struct ByRight {
+    static bool operator()(const array<int, 3> &a, const array<int, 3> &b) {
+        if (a[2] != b[2]) {
+            return a[2] < b[2];
+        } else {
+            return a[1] > b[1];
+        }
+    }
+};
+
+void solve() {
+    /*
+    - key ideas:
+        - when processing segments for the task of counting how many segments are inside the current segment,
+        if we are on a segment [l_i, r_i], we want the right end of all previous segments to be to the left
+        of the right end of our current segment
+        - when processing segments for the task of counting how many segments the current one's inside of,
+        if we are on some segment, we want the right end of all previous segments to be to the right of the
+        current segment's right end
+    - other useful ideas:
+        - also, for simplicity of implementation, we always sort things in increasing order, and if we want
+        decreasing order, we just reverse the order that we iterate over things (instead of the order that we
+        sort them in)
+    */
+
+    // gets input
+    int n;
+    cin >> n;
+    vector<array<int, 3>> segments(n);
+    for (int i = 0; i < n; ++i) {
+        segments[i][0] = i;
+        cin >> segments[i][1] >> segments[i][2];
+    }
+
+    // performs sorting and gets all index maps
+    // inverse index maps are implicitly given when running the sorting,
+    // since we store original indices with segments
+    vector<array<int, 3>> segments_by_left = segments, segments_by_right = segments;
+    sort(segments_by_left.begin(), segments_by_left.end(), ByLeft::operator());
+    sort(segments_by_right.begin(), segments_by_right.end(), ByRight::operator());
+    vector<int> left_idx(n), right_idx(n);
+    for (int i = 0; i < n; ++i) {
+        left_idx[segments_by_left[i][0]] = i;
+        right_idx[segments_by_right[i][0]] = i;
+    }
+
+    // gets number that each segment contains
+    SegmentTree st(n);
+    vector<int> contains(n, 0);
+    for (int i = 0; i < n; ++i) {
+        auto [idx, l, r] = segments_by_right[i];
+        auto l_idx = left_idx[idx];
+        contains[idx] = st.query(l_idx, n - 1);
+        st.update(l_idx, 1);
+    }
+
+    // gets number that each segment is contained in
+    st.reset();
+    vector<int> contains_us(n, 0);
+    for (int i = n - 1; i >= 0; --i) {
+        auto [idx, l, r] = segments_by_right[i];
+        auto l_idx = left_idx[idx];
+        contains_us[idx] = st.query(0, l_idx);
+        st.update(l_idx, 1);
+    }
+
+    // outputs everything
+    auto print = [](const vector<int> &v) {
+        for (int i = 0; i < v.size(); ++i) {
+            if (i) cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    };
+    print(contains);
+    print(contains_us);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
