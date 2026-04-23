a71e557f562eeac65166aaa324084ac5fd36e2d2
snkv04 <niketh.vangaru@gmail.com>
2026-04-23 14:59:23 -0400

    430B

---
+++ Codeforces/430B.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n, k, x;
+    cin >> n >> k >> x;
+    vector<pair<int, int>> segments;
+    for (int i = 0; i < n; ++i) {
+        int color;
+        cin >> color;
+        if (i == 0 || segments.back().first != color) {
+            segments.push_back(make_pair(color, 1));
+        } else {
+            segments.back().second += 1;
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < segments.size(); ++i) {
+        if (segments[i].first == x && segments[i].second == 2) {
+            int erased = 2;
+            for (int j = 1; j <= i && i + j < segments.size(); ++j) {
+                if (
+                    segments[i - j].first == segments[i + j].first &&
+                    segments[i - j].second + segments[i + j].second >= 3
+                ) {
+                    erased += segments[i - j].second + segments[i + j].second;
+                } else {
+                    break;
+                }
+            }
+            ans = max(ans, erased);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
