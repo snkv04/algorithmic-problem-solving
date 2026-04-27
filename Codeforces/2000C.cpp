ba791dd954ac20d6246e76550d84926b1be6501b
snkv04 <niketh.vangaru@gmail.com>
2026-04-27 03:53:20 -0400

    2000C

---
+++ Codeforces/2000C.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = (int) 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cin >> a;
+
+    int m;
+    cin >> m;
+    while (m--) {
+        string s;
+        cin >> s;
+        if (s.size() != a.size()) {
+            cout << "NO\n";
+            continue;
+        }
+
+        map<char, int> toint;
+        map<int, char> tochar;
+        bool valid = true;
+        for (int i = 0; i < s.size(); ++i) {
+            int num = a[i];
+            char c = s[i];
+            if (
+                (tochar.count(num) && tochar[num] != c) ||
+                (toint.count(c) && toint[c] != num)
+            ) {
+                valid = false;
+                break;
+            }
+            tochar[num] = c;
+            toint[c] = num;
+        }
+        cout << (valid ? "YES" : "NO") << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
