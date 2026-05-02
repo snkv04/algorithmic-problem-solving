b3ed52b78edecac2011af14b6feaf10d6052d8f4
snkv04 <niketh.vangaru@gmail.com>
2026-05-02 03:14:10 -0400

    CSES 2189

---
+++ CSES/2189.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll cross(pair<ll, ll> p1, pair<ll, ll> p2) {
+    return p1.first * p2.second - p1.second * p2.first;
+}
+
+void solve() {
+    ll x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    pair<ll, ll> v1 = make_pair(x2 - x1, y2 - y1), v2 = make_pair(x3 - x1, y3 - y1);
+    ll z = cross(v1, v2);
+    cout << (z == 0 ? "TOUCH" : (z > 0 ? "LEFT" : "RIGHT")) << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
