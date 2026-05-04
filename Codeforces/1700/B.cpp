9b55753a59ca27806f80d0dd16334d4d1f567360
snkv04 <niketh.vangaru@gmail.com>
2026-05-03 23:43:45 -0400

    1700B

---
+++ Codeforces/1700/B.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = n - 1; i >= 0; --i) {
+        char c;
+        cin >> c;
+        a[i] = c - '0';
+    }
+
+    if (a[n - 1] != 9) {
+        for (int i = n - 1; i >= 0; --i) {
+            cout << 9 - a[i];
+        }
+        cout << "\n";
+    } else {
+        vector<int> b(n, 0);
+        for (int i = 0; i < n; ++i) {
+            b[i] = 10 - a[i];
+        }
+        ++b[0];
+        for (int i = 0; i < n - 1; ++i) {
+            if (b[i] >= 10) {
+                b[i + 1] += b[i] / 10;
+                b[i] %= 10;
+            }
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            cout << b[i];
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
