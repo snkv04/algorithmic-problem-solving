976963cef8efbed2ebc1bdb0ec271cd8034949a8
snkv04 <niketh.vangaru@gmail.com>
2026-05-05 14:15:44 -0400

    CSES 1079

---
+++ CSES/1079.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return b * mod_pow(b, e - 1) % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+void solve(vector<ll> &fac, vector<ll> &invfac) {
+    int a, b;
+    cin >> a >> b;
+
+    ll num = fac[a];
+    ll denom = invfac[b] * invfac[a - b] % MOD;
+    cout << num * denom % MOD << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    const int MAXA = 1e6;
+    vector<ll> fac(MAXA + 1, 1), invfac(MAXA + 1, 1);
+    for (int i = 1; i <= MAXA; ++i) {
+        fac[i] = fac[i - 1] * i;
+        fac[i] %= MOD;
+    }
+    invfac[MAXA] = mod_inv(fac[MAXA]);
+    for (int i = MAXA - 1; i >= 0; --i) {
+        invfac[i] = invfac[i + 1] * (i + 1);
+        invfac[i] %= MOD;
+    }
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve(fac, invfac);
+    }
+
+    return 0;
+}
