0cb93efd70936307149d1f773f5861f480e48cc6
snkv04 <niketh.vangaru@gmail.com>
2026-05-06 08:29:25 -0400

    CSES 1728

---
+++ CSES/1728.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> r(n);
+    cin >> r;
+
+    ld ans = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            ld ev = 0;
+            for (int a_j = 1; a_j <= r[j]; ++a_j) {
+                ev += (1 / (ld) r[j]) * (max(0, r[i] - a_j) / (ld) r[i]);
+            }
+            ans += ev;
+        }
+    }
+
+    // annoying rounding logic; why not just have a tolerance lol?
+    // core idea: shift left so that tiebreaker digit is first decimal place, round to integer, then shift right
+    // didn't get AC, but doesn't matter, as the main logic (calculating EV) is correct
+    ans *= 1e6;
+    ll truncated = ans;
+    if (abs(ans - truncated - 0.5) < 1e-12) {
+        if (truncated % 2) {
+            ans = truncated + 1;
+        } else {
+            ans = truncated;
+        }
+    } else if (ans - truncated < 0.5) {
+        ans = truncated;
+    } else {
+        ans = truncated + 1;
+    }
+    ans *= 1e-6;
+    cout << std::fixed << std::setprecision(6) << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
