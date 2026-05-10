f9b10d265e4f78a42ad91ba4c1f2a63702380117
snkv04 <niketh.vangaru@gmail.com>
2026-05-10 01:12:03 -0400

    2204D

---
+++ Codeforces/2204/D.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+vector<vector<int>> adj;
+vector<int> color;
+vector<bool> visited;
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    adj = vector<vector<int>>(n);
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+        --u; --v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int ans = 0;
+    color = vector<int>(n, 0);
+    visited = vector<bool>(n, false);
+    for (int i = 0; i < n; ++i) {
+        if (!color[i]) {
+            bool bipartite = true;
+            int nodes = 0;
+            queue<int> q;
+            q.push(i);
+            color[i] = 1;
+            int ones = 1;
+            while (q.size()) {
+                int node = q.front();
+                q.pop();
+                if (visited[node]) continue;
+
+                visited[node] = true;
+                nodes += 1;
+
+                for (int next : adj[node]) {
+                    if (!color[next]) {
+                        color[next] = 3 - color[node];
+                        q.push(next);
+                        if (color[next] == 1) ones += 1;
+                    } else if (color[next] == color[node]) {
+                        bipartite = false;  // don't want to break; need to color all nodes in the component
+                    }
+                }
+            }
+
+            if (bipartite) {
+                ans += max(ones, nodes - ones);
+            }
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
