570f765bfada54a6cc92ebe826c435f325cce98c
snkv04 <niketh.vangaru@gmail.com>
2026-05-12 22:24:32 -0400

    2189C2

---
+++ Codeforces/2189C2.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int msb = 1 << 30;
+    while (!(n & msb)) {
+        msb = msb >> 1;
+    }
+
+    if (n == msb) {
+        cout << "-1\n";
+    } else if (n & 1) {
+        cout << n - 1 << " ";
+        for (int i = 2; i < n - 1; i += 2) {
+            cout << i + 1 << " " << i << " ";
+        }
+        cout << n << " 1\n";
+    } else {
+        // solution from C1
+        vector<int> a(n + 1);
+        a[1] = n;
+        a[n] = 1;
+        for (int i = 2; i < n; i += 2) {
+            a[i] = i + 1;
+            a[i + 1] = i;
+        }
+        
+        int r = n - msb;
+        swap(a[1], a[r]);
+
+        for (int i = 1; i <= n; ++i) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
