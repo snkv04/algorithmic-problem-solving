6368c0d5dda24f7d66c9ba8ec0ee08d216388af7
snkv04 <niketh.vangaru@gmail.com>
2026-05-15 03:12:52 -0400

    2202B

---
+++ Codeforces/2202B.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    int state = (n % 2) ? 0 : 1;
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == 'a') {
+            if (state == 2) {
+                cout << "NO\n";
+                return;
+            }
+            state += 1;
+        } else if (s[i] == 'b') {
+            if (state == 0) {
+                cout << "NO\n";
+                return;
+            }
+            state -= 1;
+        } else {
+            if (state == 1) {
+                if (i == n - 1 || s[i + 1] == '?') {
+                    ++i;
+                } else if (s[i + 1] == 'a') {
+                    s[i] = 'b';
+                    --state;
+                } else {
+                    s[i] = 'a';
+                    ++state;
+                }
+            } else if (state == 0) {
+                s[i] = 'a';
+                ++state;
+            } else {
+                s[i] = 'b';
+                --state;
+            }
+        }
+    }
+
+    int a = 0, b = 0;
+    for (int i = 0; i < n; ++i) {
+        a += s[i] == 'a';
+        b += s[i] == 'b';
+    }
+    if (n % 2) {
+        cout << (abs(a - b) <= 1 ? "YES" : "NO") << "\n";
+    } else {
+        cout << (a == b ? "YES" : "NO") << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
