f715e7d5fdad18a0b279a7d291ee57007e5a2468
snkv04 <niketh.vangaru@gmail.com>
2026-05-20 11:17:59 -0400

    2158D

---
+++ Codeforces/2158C.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return mod_pow(b, e - 1) * b % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+ll mod_div(ll n, ll d) {
+    return n * mod_inv(d) % MOD;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<ll> a(n), b(n);
+    cin >> a >> b;
+
+    vector<ll> left(n), right(n);
+    left[0] = a[0];
+    for (int i = 1; i < n; ++i) {
+        left[i] = max(a[i], left[i - 1] + a[i]);
+    }
+    right[n - 1] = a[n - 1];
+    for (int i = n - 2; i >= 0; --i) {
+        right[i] = max(a[i], right[i + 1] + a[i]);
+    }
+
+    if (k % 2) {
+        ll ans = -1e16;
+        for (int i = 0; i < n; ++i) {
+            ll sum = a[i] + b[i];
+            if (i) sum += max(0LL, left[i - 1]);
+            if (i < n - 1) sum += max(0LL, right[i + 1]);
+            ans = max(ans, sum);
+        }
+        cout << ans << endl;
+    } else {
+        ll ans = -1e16;
+        for (int i = 0; i < n; ++i) ans = max(ans, left[i]);
+        cout << ans << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
