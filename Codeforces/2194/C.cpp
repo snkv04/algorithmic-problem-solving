05d3dbd4dbff0a6aaa9d4ef12a483f4593909634
snkv04 <niketh.vangaru@gmail.com>
2026-05-20 23:56:53 -0400

    2194C

---
+++ Codeforces/2194/C.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return mod_pow(b, e - 1) * b % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+ll mod_div(ll n, ll d) {
+    return n * mod_inv(d) % MOD;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<string> s(k);
+    cin >> s;
+
+    vector<set<char>> avail(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < k; ++j) {
+            avail[i].insert(s[j][i]);
+        }
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        if (n % i) continue;
+
+        bool valid = true;
+        stringstream ss;
+        for (int start = 0; start < i; ++start) {
+            set<char> curr = avail[start];
+            for (int j = start + i; j < n; j += i) {
+                set<char> newcurr = curr;
+                for (char c : curr) {
+                    if (!avail[j].count(c)) {
+                        newcurr.erase(c);
+                    }
+                }
+                curr = std::move(newcurr);
+                if (curr.empty()) break;
+            }
+
+            if (curr.empty()) {
+                valid = false;
+                break;
+            } else {
+                ss << *curr.begin();
+            }
+        }
+
+        if (valid) {
+            int times = n / i;
+            stringstream ans;
+            while (times--) ans << ss.str();
+            cout << ans.str() << endl;
+            return;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
