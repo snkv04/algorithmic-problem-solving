1436566fb766469e91563cb67cdfb9d59ecd5e49
snkv04 <niketh.vangaru@gmail.com>
2026-05-26 23:12:16 -0700

    1824B2

---
+++ Codeforces/1824B2.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7; // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+int n, k;
+vector<ll> fac, inv_fac;
+vector<vector<int>> adj;
+vector<int> subtree_size;
+ll ans;
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return mod_pow(b, e - 1) * b % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+ll mod_div(ll n, ll d) {
+    return n * mod_inv(d) % MOD;
+}
+
+ll nCr(ll n, ll r) {
+    if (r > n) return 0;
+
+    ll num = fac[n];
+    ll denom = inv_fac[r] * inv_fac[n - r] % MOD;
+    return num * denom % MOD;
+}
+
+void dfs(int node, int parent) {
+    subtree_size[node] = 1;
+    for (int next : adj[node]) {
+        if (next == parent) continue;
+
+        dfs(next, node);
+        subtree_size[node] += subtree_size[next];
+
+        // never does this for the root, to only count edges
+        ans += nCr(subtree_size[next], k / 2) * nCr(n - subtree_size[next], k / 2) % MOD;
+        ans %= MOD;
+    }
+}
+
+void solve() {
+    // reads in input
+    cin >> n >> k;
+    if (k & 1) {
+        cout << "1\n";
+        return;
+    }
+
+    // does some precomputing
+    fac = vector<ll>(n + 1, 1);
+    inv_fac = fac;
+    for (int i = 1; i <= n; ++i) {
+        fac[i] = (fac[i - 1] * i) % MOD;
+    }
+    inv_fac[n] = mod_inv(fac[n]);
+    for (int i = n - 1; i >= 1; --i) {
+        inv_fac[i] = (inv_fac[i + 1] * (i + 1)) % MOD;
+    }
+
+    // reads in graph
+    adj.resize(n);
+    for (int i = 0; i < n - 1; ++i) {
+        int a, b;
+        cin >> a >> b;
+        --a; --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    // computes answer
+    subtree_size = vector<int>(n, 0);
+    ans = 0;
+    dfs(0, -1);  // gets the sum
+    ans = (ans * mod_inv(nCr(n, k))) % MOD;  // divides by denominator
+    ans = (ans + 1) % MOD;  // moves from EV of "good edges" to EV of "good nodes"
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
