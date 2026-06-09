57d4c2b8d13da7e41031195fe4220a2add01e022
snkv04 <niketh.vangaru@gmail.com>
2026-06-08 21:11:39 -0700

    2151D

---
+++ Codeforces/2151/D.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+vector<ll> fac;
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return mod_pow(b, e - 1) * b % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+ll mod_div(ll n, ll d) {
+    return n * mod_inv(d) % MOD;
+}
+
+ll nCr(ll n, ll r) {
+    ll num = fac[n];
+    ll denom = fac[r] * fac[n - r] % MOD;
+    return mod_div(num, denom);
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cin >> a;
+    if (std::accumulate(a.begin(), a.end(), 0LL) != n || std::accumulate(a.begin() + (n + 1) / 2, a.end(), 0LL) != 0) {
+        cout << "0\n";
+        return;
+    }
+
+    fac = vector<ll>(n + 1, 1);
+    for (int i = 1; i <= n; ++i) {
+        fac[i] = (fac[i - 1] * i) % MOD;
+    }
+
+    ll ans = 1;
+    int available = n % 2;
+    for (int i = n / 2; i >= 0; --i) {
+        if (a[i] > available) {
+            cout << "0\n";
+            return;
+        }
+
+        ans = ans * nCr(available, a[i]) % MOD;
+        available -= a[i];
+        available += 2;
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
