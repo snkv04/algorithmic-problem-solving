ddcf1ce3bec3f0cecf66cd822b6555c7ebee13f2
snkv04 <niketh.vangaru@gmail.com>
2026-06-12 22:52:16 -0700

    Reimplemented LC 239

--- LeetCode/Hard/239.cpp
+++ LeetCode/Hard/239.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> result(n - k + 1, 0);
-        deque<pair<int, int>> d;
-        for (int i = 0; i < n; ++i) {
-            if (i >= k && d.size() && d.front().second <= i - k) {
+        // basic idea: we want a decreasing monotonic stack, where the
+        // top element is the back (right) of the deque, but then at each
+        // index, we care about the bottom of the stack (the max element)
+
+        // initializes the deque before the first full window
+        deque<int> d;
+        auto push = [&](int idx) {
+            // removes the bottom of the stack if it's too far left
+            // of the current index
+            if (d.size() && d.front() == idx - k)
                 d.pop_front();
-            }
 
-            while (d.size() && d.back().first <= nums[i]) {
+            // enforces the monotonic stack property of being strictly
+            // (though maybe strictness is not important here) decreasing
+            while (d.size() && nums[d.back()] <= nums[idx])
                 d.pop_back();
-            }
-            d.push_back(make_pair(nums[i], i));
 
-            if (i >= k-1) {
-                result[i-k+1] = d.front().first;
-            }
+            // pushes the current index onto the stack
+            d.push_back(idx);
+        };
+        for (int i = 0; i < k - 1; ++i) push(i);
+
+        // iterates through each window, and for each, gets the bottom
+        // of the stack
+        vector<int> ans;
+        for (int i = k - 1; i < nums.size(); ++i) {
+            push(i);
+            ans.push_back(nums[d.front()]);
         }
-        return result;
+        return ans;
     }
 };
