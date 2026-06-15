f5d7a72ad5a2038d5acc580bcb41c677d4a86ecd
snkv04 <niketh.vangaru@gmail.com>
2026-06-14 22:19:38 -0700

    CSES 2076

---
+++ CSES/2076.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7;  // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+vector<vector<int>> adj;
+vector<int> pre, low, scc;
+int timer;
+stack<int> stk;
+vector<bool> onstk;
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T1, typename T2>
+std::istream& operator>>(std::istream &is, std::pair<T1, T2> &p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return mod_pow(b, e - 1) * b % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+ll mod_div(ll n, ll d) {
+    return n * mod_inv(d) % MOD;
+}
+
+void dfs(int node, int parent) {
+    pre[node] = low[node] = timer++;
+    stk.push(node);
+    onstk[node] = true;
+
+    for (int next : adj[node]) {
+        if (next == parent) continue;
+
+        if (pre[next] == -1) dfs(next, node);
+        if (onstk[next]) low[node] = min(low[node], low[next]);
+    }
+
+    if (pre[node] == low[node]) {
+        int top = -1;
+        do {
+            top = stk.top();
+            stk.pop();
+            onstk[top] = false;
+            scc[top] = node;
+        } while (top != node);
+    }
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    adj.resize(n);
+    vector<pair<int, int>> edges;
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+        --a; --b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+        edges.push_back({a, b});
+    }
+
+    timer = 0;
+    pre.resize(n); fill(pre.begin(), pre.end(), -1);
+    low = scc = pre;
+    onstk.resize(n); fill(onstk.begin(), onstk.end(), 0);
+    dfs(0, -1);
+
+    vector<pair<int, int>> ans;
+    for (auto [a, b] : edges) {
+        if (scc[a] != scc[b]) {
+            ans.push_back({a, b});
+        }
+    }
+
+    cout << ans.size() << "\n";
+    for (auto [a, b] : ans) {
+        cout << a + 1 << " " << b + 1 << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
