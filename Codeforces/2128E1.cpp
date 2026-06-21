886b58a552340c23767677a62adb6ec804ef97e4
snkv04 <niketh.vangaru@gmail.com>
2026-06-21 01:56:35 -0700

    2128E1

---
+++ Codeforces/2128E1.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7;  // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+int n, k, ans, ansl, ansr;
+vector<int> a;
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T1, typename T2>
+std::istream& operator>>(std::istream &is, std::pair<T1, T2> &p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return mod_pow(b, e - 1) * b % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+ll mod_div(ll n, ll d) {
+    return n * mod_inv(d) % MOD;
+}
+
+bool check(int threshold) {
+    vector<int> cnt(n + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+        cnt[i] = cnt[i - 1];
+        if (a[i - 1] >= threshold) {
+            cnt[i] += 1;
+        }
+    }
+
+    vector<int> bestlval(2, -1e9), bestl(2, -1);
+    for (int i = k; i <= n; ++i) {
+        int adding = i - k + 1;
+        if (adding - 2 * cnt[adding - 1] > bestlval[adding % 2]) {
+            bestlval[adding % 2] = adding - 2 * cnt[adding - 1];
+            bestl[adding % 2] = adding;
+        }
+
+        if (bestlval[(i % 2) ^ 1] >= i - 2 * cnt[i] + 1) {
+            ans = threshold;
+            ansr = i;
+            ansl = bestl[(i % 2) ^ 1];
+            return true;
+        }
+        if (bestlval[i % 2] >= i - 2 * cnt[i] + 2) {
+            ans = threshold;
+            ansr = i;
+            ansl = bestl[i % 2];
+            return true;
+        }
+    }
+    return false;
+}
+
+void solve() {
+    cin >> n >> k;
+    a = vector<int>(n);
+    cin >> a;
+
+    int lo = 1, hi = n;
+    ans = -1; ansl = -1; ansr = -1;
+    while (lo <= hi) {
+        int mid = (lo + hi) / 2;
+        if (check(mid)) {
+            lo = mid + 1;
+        } else {
+            hi = mid - 1;
+        }
+    }
+    cout << ans << " " << ansl << " " << ansr << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
