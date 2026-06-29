0f6c5dbcb9c23cdbaf6735f0573a01d2aceee8d2
snkv04 <niketh.vangaru@gmail.com>
2026-06-28 23:03:21 -0700

    2232 A, B, C, and D

---
+++ Codeforces/2232/D.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7;  // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+vector<int> disks;
+vector<array<int, 3>> ans;
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T1, typename T2>
+std::istream& operator>>(std::istream &is, std::pair<T1, T2> &p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return mod_pow(b, e - 1) * b % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+ll mod_div(ll n, ll d) {
+    return n * mod_inv(d) % MOD;
+}
+
+void solve_disk(int disk, int start, int end, vector<int> &disks) {
+    if (disk < 0) return;
+
+    int other = start ^ end;
+    if (disks[disk]) {
+        solve_disk(disk - disks[disk] - 1, start, other, disks);
+        ans.push_back({disk, start, end});
+        solve_disk(disk - disks[disk] - 1, other, start, disks);
+        solve_disk(disk - 1, start, end, disks);
+    } else {
+        solve_disk(disk - 1, start, other, disks);
+        ans.push_back({disk, start, end});
+        solve_disk(disk - 1, other, end, disks);
+    }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    disks = vector<int>(n);
+    cin >> disks;
+    for (int i = 0; i < n; ++i) {
+        if (disks[i] > i) {
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    ans.clear();
+    solve_disk(n - 1, 1, 3, disks);
+    cout << "YES\n" << ans.size() << "\n";
+    for (auto [disk, start, end] : ans) {
+        cout << disk + 1 << " " << start << " " << end << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
