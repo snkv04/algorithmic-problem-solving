801d48942198e663424e4be8d431005aa22f2054
snkv04 <niketh.vangaru@gmail.com>
2026-07-01 01:53:22 -0700

    2233E2

---
+++ Codeforces/2233/E2.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int MOD = 1e9 + 7;  // 998244353;
+int di[] = {0, 0, 1, -1}, dj[] = {1, -1, 0, 0};
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream &os, const std::array<T, N> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &c) {
+    os << "[";
+    for (const auto &elem : c) {
+        os << elem << ",";
+    }
+    os << "]";
+    return os;
+}
+
+template <typename T1, typename T2>
+std::istream& operator>>(std::istream &is, std::pair<T1, T2> &p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
+template <typename T, size_t N>
+std::istream& operator>>(std::istream &is, std::array<T, N> &a) {
+    for (size_t i = 0; i < N; ++i) {
+        is >> a[i];
+    }
+    return is;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &v) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        is >> v[i];
+    }
+    return is;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    ll A = max(a, b), B = min(a, b);
+    while (B != 0) {
+        ll R = A % B;
+        A = B;
+        B = R;
+    }
+    return A;
+}
+
+ll lcm(ll a, ll b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    return a / gcd(a, b) * b;
+}
+
+ll mod_pow(ll b, ll e) {
+    if (e == 0) return 1;
+    if (e % 2) return mod_pow(b, e - 1) * b % MOD;
+    else return mod_pow(b * b % MOD, e / 2) % MOD;
+}
+
+ll mod_inv(ll x) {
+    return mod_pow(x, MOD - 2);
+}
+
+ll mod_div(ll n, ll d) {
+    return n * mod_inv(d) % MOD;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    int b = std::ceil(std::log2(n + 1));
+    vector<string> lines(b);
+    cin >> lines;
+
+    auto cnt1s = [](const string &s) {
+        int cnt = 0;
+        for (char c : s) cnt += c == '1';
+        return cnt;
+    };
+    sort(lines.begin(), lines.end(), [&](const string &a, const string &b) {
+        return cnt1s(a) < cnt1s(b);
+    });
+
+    set<int> used;
+    for (int i = 0; i < n; ++i) {
+        int val = 0;
+        for (int j = 0; j < b; ++j) {
+            val += (lines[j][i] == '1') * (1 << (b - 1 - j));
+        }
+        used.insert(val);
+    }
+    for (int i = 1; i <= n; ++i) {
+        if (used.count(i) == 0) {
+            cout << "0\n";
+            return;
+        }
+    }
+
+    vector<ll> fac(b + 1, 1);
+    for (int i = 1; i <= b; ++i) fac[i] = fac[i - 1] * i;
+    vector<int> cnt(n + 1, 0);
+    for (int j = 0; j < b; ++j) {
+        cnt[cnt1s(lines[j])] += 1;
+    }
+
+    ll ans = 1;
+    for (int i = 1; i <= n; ++i) {
+        ans *= fac[cnt[i]];
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
